8c6ef61dca8b02e60797f85e99ed47835ed6e00b
Kaden Wilkinson <7799267+kdawgwilk@users.noreply.github.com>
2021-07-07 18:09:14 -0600

    Fix any scalar missing parse/serialize (#5)

--- lib/absinthe/federation/types.ex
+++ lib/absinthe/federation/types.ex
@@ -4,7 +4,10 @@ defmodule Absinthe.Federation.Types do
   use Absinthe.Schema.Notation
 
   @desc "The _Any scalar is used to pass representations of entities from external services into the root _entities field for execution."
-  scalar :any, name: "_Any"
+  scalar :any, name: "_Any" do
+    parse fn value -> {:ok, value} end
+    serialize fn value -> value end
+  end
 
   object :service, name: "_Service" do
     field :sdl, :string
