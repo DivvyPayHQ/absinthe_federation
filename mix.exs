58e79c5bf37012ecc383233dec33bba02670960c
Kaden Wilkinson <wilkinson.kaden@gmail.com>
2021-09-24 17:07:52 -0600

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Absinthe.Federation.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/DivvyPayHQ/absinthe_federation"
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [
