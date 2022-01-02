ae812a6b8af17dfa3fefc898b003b48b699035eb
scottming <therealscottming@gmail.com>
2022-01-02 08:26:49 +0800

    Add nested key failed test

--- test/absinthe/federation/schema/key_fields_must_be_valid_test.exs
+++ test/absinthe/federation/schema/key_fields_must_be_valid_test.exs
@@ -1,8 +1,8 @@
 defmodule Absinthe.Federation.Schema.KeyFieldsMustBeValidTest do
   use Absinthe.Federation.Case, async: true
 
-  @invalid_schema """
-    defmodule FieldNotExistSchema do
+  @flat_key_schema """
+    defmodule FlatKeySchema do
       use Absinthe.Schema
       use Absinthe.Federation.Schema
 
@@ -10,16 +10,38 @@ defmodule Absinthe.Federation.Schema.KeyFieldsMustBeValidTest do
         extends()
       end
 
-      object :dog do
-        key_fields(["uuid"])
+      object :product do
+        key_fields(["uuid", "name"])
         field :id, non_null(:id)
       end
     end
   """
 
-  @tag :impl
-  test "it should throw an error" do
-    error = ~r/The @key \"uuid\" is not exist in :dog object./
-    assert_raise(Absinthe.Schema.Error, error, fn -> Code.eval_string(@invalid_schema) end)
+  @nested_key_schema """
+    defmodule NestedKeySchema do
+      use Absinthe.Schema
+      use Absinthe.Federation.Schema
+
+      object :product_variation do
+          field :id, non_null(:id)
+      end
+
+      object :product do
+        key_fields(["upc", "sku variation { id }"])
+        field :upc, non_null(:string)
+        field :sku, non_null(:string)
+        field :variation, non_null(:product_variation)
+      end
+    end
+  """
+
+  test "it should throw an error when flat key fields not exist" do
+    error = ~r/The @key \"name\" is not exist in :product object./
+    assert_raise(Absinthe.Schema.Error, error, fn -> Code.eval_string(@flat_key_schema) end)
+  end
+
+  test "it should throw an error when nested key fields not exist" do
+    error = ~r/The @key \"name\" is not exist in :product object./
+    assert_raise(Absinthe.Schema.Error, error, fn -> Code.eval_string(@nested_key_schema) end)
   end
 end
