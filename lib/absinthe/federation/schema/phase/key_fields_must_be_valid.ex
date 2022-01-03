9b4698fda4b518d5ba58624b93964fff3180ef6e
scottming <therealscottming@gmail.com>
2022-01-03 22:45:58 +0800

    Add some nested logic

--- lib/absinthe/federation/schema/phase/key_fields_must_be_valid.ex
+++ lib/absinthe/federation/schema/phase/key_fields_must_be_valid.ex
@@ -38,11 +38,33 @@ defmodule Absinthe.Federation.Schema.Phase.KeyFieldsMustBeValid do
   end
 
   defp validate_key_fields(key_fields, object) when is_binary(key_fields) do
-    if key_fields |> in?(object.fields) do
+    case String.split(key_fields) do
+      [key_fields] ->
+        if key_fields |> in?(object.fields) do
+          object
+        else
+          Absinthe.Phase.put_error(object, error(key_fields, object))
+        end
+
+      [_ | _] ->
+        nested_key_selections = key_fields |> parse_nested_key()
+        Enum.reduce(nested_key_selections, object, fn x, acc -> validate_nested_key(x, acc, key_fields) end)
+    end
+  end
+
+  defp validate_nested_key(%{selection_set: nil, name: key}, object, key_fields) do
+    # when key in current schema
+    if key |> in?(object.fields) do
       object
     else
-      Absinthe.Phase.put_error(object, error(key_fields, object))
+      Absinthe.Phase.put_error(object, error(key, object, key_fields))
     end
+  end
+
+  defp validate_nested_key(selection, object, _key_fields) do
+    bp = object.module.__absinthe_blueprint__()
+    Absinthe.Blueprint.Schema.lookup_type(bp, String.to_atom(selection.name))
+    object
   end
 
   defp in?(key, fields) do
@@ -59,9 +81,32 @@ defmodule Absinthe.Federation.Schema.Phase.KeyFieldsMustBeValid do
     }
   end
 
+  defp error(key, object, key_fields) do
+    %Absinthe.Phase.Error{
+      message: explanation(key, object, key_fields),
+      locations: [object.__reference__.location],
+      phase: __MODULE__,
+      extra: %{key: key}
+    }
+  end
+
+  defp parse_nested_key(nested_key) do
+    with {:ok, tokens} <- Absinthe.Lexer.tokenize("{ " <> nested_key <> " }"),
+         {:ok, parsed} <- :absinthe_parser.parse(tokens) do
+      access = [Access.key(:definitions), Access.at(0), Access.key(:selection_set), Access.key(:selections)]
+      get_in(parsed, access)
+    end
+  end
+
   def explanation(key, object) do
     """
     The @key #{inspect(key)} is not exist in #{inspect(object.identifier)} object.
+    """
+  end
+
+  def explanation(field, _object, key_fields) do
+    """
+    The field #{inspect(field)} of @key #{inspect(key_fields)} is not exist.
     """
   end
 end
--- test/absinthe/federation/schema/key_fields_must_be_valid_test.exs
+++ test/absinthe/federation/schema/key_fields_must_be_valid_test.exs
@@ -22,12 +22,38 @@ defmodule Absinthe.Federation.Schema.KeyFieldsMustBeValidTest do
       use Absinthe.Schema
       use Absinthe.Federation.Schema
 
+      query do
+        extends()
+      end
+
       object :product_variation do
-          field :id, non_null(:id)
+        field :id, non_null(:id)
       end
 
       object :product do
-        key_fields(["upc", "sku variation { id }"])
+        key_fields("uuid variation { id }")
+        field :upc, non_null(:string)
+        field :sku, non_null(:string)
+        field :variation, non_null(:product_variation)
+      end
+    end
+  """
+
+  @nested_ref_key_schema """
+    defmodule NestedRefKeySchema do
+      use Absinthe.Schema
+      use Absinthe.Federation.Schema
+
+      query do
+        extends()
+      end
+
+      object :product_variation do
+        field :uuid, non_null(:id)
+      end
+
+      object :nested_product do
+        key_fields("uuid variation { id }")
         field :upc, non_null(:string)
         field :sku, non_null(:string)
         field :variation, non_null(:product_variation)
@@ -36,12 +62,20 @@ defmodule Absinthe.Federation.Schema.KeyFieldsMustBeValidTest do
   """
 
   test "it should throw an error when flat key fields not exist" do
-    error = ~r/The @key \"name\" is not exist in :product object./
+    error =
+      ~r/The @key \"name\" is not exist in :product object.(\w|\W|\s)+The @key \"uuid\" is not exist in :product object./
+
     assert_raise(Absinthe.Schema.Error, error, fn -> Code.eval_string(@flat_key_schema) end)
   end
 
-  test "it should throw an error when nested key fields not exist" do
-    error = ~r/The @key \"name\" is not exist in :product object./
+  test "it should throw an error when nested key fields not exist in object" do
+    error = ~r/The field \"uuid\" of @key \"uuid variation { id }\" is not exist./
     assert_raise(Absinthe.Schema.Error, error, fn -> Code.eval_string(@nested_key_schema) end)
+  end
+
+  @tag :impl
+  test "it should throw an error when nested key fields not exist in schema" do
+    error = ~r/The field \"uuid\" of @key \"uuid variation { id }\" is not exist./
+    assert_raise(Absinthe.Schema.Error, error, fn -> Code.eval_string(@nested_ref_key_schema) end)
   end
 end
